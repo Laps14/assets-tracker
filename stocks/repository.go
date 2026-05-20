999e641438e5b804beedcbd71f9dc5d857ada470
Laps14 <lucas.ramiresestudos@gmail.com>
2026-05-20 00:32:42 -0400

    feat: Containerized Postgres and CRUD
    Now it's available the implementation of a CRUD to a containerized
    PostgreSQL database. Still, there's no delete for the already notified
    stocks, because I still need to think how I will treat a stock as
    already notified. Since there's some cases where the user will want to
    make the notification persistent.
    
    Also, I've implemented the show function, which will show an image of
    the company's logo in the terminal and, also, will show some info about
    it (stock val, description, target vals etc.)

--- stocks/repository.go
+++ stocks/repository.go
@@ -3,14 +3,15 @@ package stocks
 import "context"
 
 type Reader interface {
-	Select(ctx context.Context, id uint64) (*Stock, error)
+	Select(ctx context.Context, id int64) (*Stock, error)
 	SelectAll(ctx context.Context) ([]*Stock, error)
 }
 
 type Writer interface {
-	Insert(ctx context.Context, stock *Stock) (uint64, error)
+	Insert(ctx context.Context, stock *Stock) (int64, error)
 	Update(ctx context.Context, stock *Stock) error
-	Delete(ctx context.Context, id uint64) error
+	Delete(ctx context.Context, id int64) error
+	DeleteAll(ctx context.Context) error
 }
 
 type Repository interface {
